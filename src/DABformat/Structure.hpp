995707d7b1b72fab4171ebe1ab51f49af6e8a224
Jack Zautner <jackzautner@gmail.com>
2022-10-16 14:20:50 -0500

    Finished setting up dabFile structure

--- src/DABformat/Structure.hpp
+++ src/DABformat/Structure.hpp
@@ -16,19 +16,19 @@ namespace DABnetUtils {
             public:
 
                 struct portConfig {
-                    unsigned short portID;
-                    strainType type;
-                    std::string strainName;
-                    float voltage;
+                    unsigned short portID;  // ID of this port
+                    strainType type;        // Type of strain for this port
+                    std::string strainName; // Name of strain for this port
+                    float voltage;          // Voltage given to this port
                 };
             
                 struct dabFile {
-                    unsigned int fileSize;
-                    unsigned short version;
-                    std::string name;
-                    std::string author;
-                    std::vector<portConfig> portConfigs;
-                    char magic[];
+                    unsigned int fileSize;               // Size of file
+                    unsigned short version;              // Version of config
+                    std::string name;                    // Name of config
+                    std::string author;                  // Name of user who made this config
+                    std::vector<portConfig> portConfigs; // Configurations for each individual port
+                    char magic[3] = { 'D', 'A', 'B' };   // Magic array for identifying the .dab binary
                 };
 
         };
