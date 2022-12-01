ae41e78b8550450090513839dd96266d96f12e93
Jack Zautner <jackzautner@gmail.com>
2022-12-01 10:29:36 -0600

    Updated dab file structure (contains 1 error)

--- src/DABformat/Structure.hpp
+++ src/DABformat/Structure.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file Structure.hpp
+ * @author Jack Zautner
+ * @date 1 Dec 2022
+ * 
+ * @brief Structure for the .dab binary
+ */
+#ifndef STRUCTURE_GUARD
+#define STRUCTURE_GUARD
+#endif
+
 #include <string>
 #include <vector>
 
@@ -5,6 +16,11 @@ namespace DABnetUtils {
 
     namespace DABformat {
 
+        /**
+         * @brief defines types of strains for advanced port configurations.
+         * 
+         * @deprecated no support as of now. Will possibly change this in the future
+         */
         enum class strainType {
             INDICA,
             SATIVA,
@@ -15,6 +31,11 @@ namespace DABnetUtils {
 
             public:
 
+                /**
+                 * @brief structure to hold configurations for specific ports
+                 * 
+                 * @deprecated no support as of now. Will possibly change this in the future
+                 */
                 struct portConfig {
                     unsigned short portID;  // ID of this port
                     strainType type;        // Type of strain for this port
@@ -22,14 +43,19 @@ namespace DABnetUtils {
                     float voltage;          // Voltage given to this port
                 };
             
+                /**
+                 * @brief structure that will be converted to a .dab binary 
+                 * 
+                 * @bug it doesn't fucking run
+                 */
                 struct dabFile {
-                    unsigned int fileSize;               // Size of file
-                    unsigned short version;              // Version of config
-                    std::string name;                    // Name of config
-                    std::string author;                  // Name of user who made this config
-                    std::vector<portConfig> portConfigs; // Configurations for each individual port
-                    unsigned int numPorts;               // Number of ports occupied by cartriges
-                    char magic[3] = { 'D', 'A', 'B' };   // Magic array for identifying the .dab binary
+                    unsigned int fileSize;             // Size of file
+                    std::uint16_t version;             // Version of config
+                    std::string author;                // Name of user who made this config
+                    std::string name;                  // Name of config 
+                    std::uint8_t numPorts;             // Number of ports occupied by cartriges
+                    float voltages[numPorts];          // Array containing a voltage for each port (ERROR)
+                    char magic[3] = { 'D', 'A', 'B' }; // Magic array for identifying the .dab binary
                 };
 
         };
